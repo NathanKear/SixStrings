4ecaf54054a1b026f438569d7784dafdaa42efc0
Nathan Kear <n.j.kear@gmail.com>
2019-05-01 20:51:17 +1200

    Add click listener to song list items

--- app/src/main/java/nk/sixstrings/ui/songlist/SongListFragment.kt
+++ app/src/main/java/nk/sixstrings/ui/songlist/SongListFragment.kt
@@ -19,6 +19,18 @@ class SongListFragment : Fragment() {
 
     private lateinit var viewModel: SongListViewModel
     private val songs = listOf(
+        Song("Wish You Were Here", "Pink Floyd"),
+        Song("Airbag", "Radiohead"),
+        Song("Cycling Trivialities", "José González"),
+        Song("With The Ink Of A Ghost", "José González"),
+        Song("Viðrar vel til loftárása", "Sigur Rós"),
+        Song("Ágætis byrjun", "Sigur Rós"),
+        Song("Perth", "Bon Iver"),
+        Song("Brokeback Mountain Theme", "Gustavo Santaolalla"),
+        Song("Champagne Supernova", "Oasis"),
+        Song("The Last Of Us Theme", "Gustavo Santaolalla"),
+        Song("Caring Is Creepy", "The Shins"),
+        Song("New Slang", "The Shins"),
         Song("Feeling You", "Harrison Storm"),
         Song("Mean To Me", "Stella Donnelly"),
         Song("The Barrel", "Aldous Harding"),
@@ -38,8 +50,12 @@ class SongListFragment : Fragment() {
 
         song_list_recycler_view.let {
             it.layoutManager = LinearLayoutManager(context)
-            it.adapter = SongListRecyclerViewAdapter(songs, context!!)
+            it.adapter = SongListRecyclerViewAdapter(songs, ::onSongItemClicked, context!!)
         }
+    }
+
+    private fun onSongItemClicked(view: View) {
+        
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/nk/sixstrings/ui/songlist/SongListRecyclerViewAdapter.kt
+++ app/src/main/java/nk/sixstrings/ui/songlist/SongListRecyclerViewAdapter.kt
@@ -5,24 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recyclerview_row.view.*
+import kotlinx.android.synthetic.main.song_list_item_row.view.*
 import nk.sixstrings.R
 
-class SongListRecyclerViewAdapter(private val songs: List<Song>, private val context: Context) : RecyclerView.Adapter<SongListRecyclerViewAdapter.SongViewHolder>() {
+class SongListRecyclerViewAdapter(private val songs: List<Song>, private val onClickListener: (View) -> Unit, private val context: Context) : RecyclerView.Adapter<SongListRecyclerViewAdapter.SongViewHolder>() {
 
     override fun getItemCount() = songs.size
 
-    override fun onBindViewHolder(holder: SongViewHolder, position: Int) = holder.bind(songs[position])
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) = holder.bind(songs[position], onClickListener)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val inflater = LayoutInflater.from(context)
-        return SongViewHolder(inflater.inflate(R.layout.recyclerview_row, parent, false))
+        return SongViewHolder(inflater.inflate(R.layout.song_list_item_row, parent, false))
     }
 
     class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(song: Song) = with(itemView) {
-            song_name.text = song.name
-            song_artist.text = song.artist
+        fun bind(song: Song, onClickListener: (View) -> Unit) = with(itemView) {
+                    song_name.text = song.name
+                    song_artist.text = song.artist
+                    setOnClickListener(onClickListener)
         }
     }
 }
